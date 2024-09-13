4515eb0081611d1d2b99844e5278a4f6425a5d56
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-09-13 13:12:26 -0700

    update entrypoint to just enter into the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -l
 set -e
-pwd
-case $1 in 
-  "build")
-      Rscript -e "corefast::build('site')"
-  ;;
-  "hello")
-      Rscript -e "corefast::hello()"
-  ;;
-  *)
-      echo "nothing here!"
-  ;;
-esac
-echo "Action complete!"
+# pwd
+# case $1 in 
+#   "build")
+#       Rscript -e "corefast::build('site')"
+#   ;;
+#   "hello")
+#       Rscript -e "corefast::hello()"
+#   ;;
+#   *)
+#       echo "nothing here!"
+#   ;;
+# esac
+
+echo "use `Rscript -e \"corefast::build('site')\"` to build!"
+bash
