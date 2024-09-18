dcf44571ff76aad0edd6506d1face72e67944bb9
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-09-18 14:23:50 -0700

    functionalize writer

--- appHelper.py
+++ appHelper.py
@@ -24,6 +24,28 @@ def read_pem():
         cert = pemf
     return cert
 
+def write_string(key, value):
+    # assign them to the GitHub output (which is a file whose path is assigned
+    # to the GITHUB_OUTPUT variable) 
+    ghoutput = os.environ.get("GITHUB_OUTPUT")
+    if ghoutput is None:
+        throwException("oops")
+    if os.path.isfile(ghoutput):
+        with open(ghoutput, 'a') as f:
+            f.write(f'{key}={value}\n')
+            f.close()
+
+def write_json(key, value):
+    # assign them to the GitHub output (which is a file whose path is assigned
+    # to the GITHUB_OUTPUT variable) 
+    ghoutput = os.environ.get("GITHUB_OUTPUT")
+    if ghoutput is None:
+        throwException("oops")
+    rid = id_generator()
+    if os.path.isfile(ghoutput):
+        with open(ghoutput, 'a') as f:
+            f.write(f'{key}<<{rid}\n{json.dumps(value)}\n{rid}\n')
+            f.close()
 
 def get_installation():
     # Auth dance
@@ -39,17 +61,9 @@ def get_slug_id():
     app = ghapp.get_app()
     app_usr = gh.get_user(app.slug+"[bot]")
     email = f'{app_usr.id}+{app_usr.login}@users.noreply.github.com'
-    # assign them to the GitHub output (which is a file whose path is assigned
-    # to the GITHUB_OUTPUT variable) 
-    ghoutput = os.environ.get("GITHUB_OUTPUT")
-    if ghoutput is None:
-        throwException("oops")
-    if os.path.isfile(ghoutput):
-        with open(ghoutput, 'a') as f:
-            f.write(f'slug={app.slug}\n')
-            f.write(f'id={app_usr.id}\n')
-            f.write(f'email={email}\n')
-            f.close()
+    write_string("slug", app.slug)
+    write_string("email", email)
+    write_string("id", app_usr.id)
 
 
 def list_repositories():
@@ -57,30 +71,11 @@ def list_repositories():
     installation = ghapp.get_installations()[0]
     # get the full names for the repositories
     repos = [x.full_name for x in installation.get_repos()]
-
-    # assign them to the GitHub output (which is a file whose path is assigned
-    # to the GITHUB_OUTPUT variable) 
-    ghoutput = os.environ.get("GITHUB_OUTPUT")
-    if ghoutput is None:
-        throwException("oops")
-    rid = id_generator()
-    if os.path.isfile(ghoutput):
-        with open(ghoutput, 'a') as f:
-            f.write(f'repos<<{rid}\n{json.dumps(repos)}\n{rid}\n')
-            f.close()
+    write_json("repos", repos)
 
 def get_token():
     ghapp = get_installation()
     installation = ghapp.get_installations()[0]
     token = ghapp.get_access_token(installation.id)
-    # assign them to the GitHub output (which is a file whose path is assigned
-    # to the GITHUB_OUTPUT variable) 
-    ghoutput = os.environ.get("GITHUB_OUTPUT")
-    if ghoutput is None:
-        throwException("oops")
-    rid = id_generator()
-    if os.path.isfile(ghoutput):
-        with open(ghoutput, 'a') as f:
-            f.write(f'token={token.token}\n')
-            f.close()
+    write_string("token", token.token)
 
