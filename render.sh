0c3bf09efe86411edfcd87b5e9f68bf1a8348857
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-09-27 13:43:09 -0700

    try to unfuck this

--- render.sh
+++ render.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p /site/pages/
-cp /site/*md /site/pages/
-cp -R /static/* /site/pages/
-quarto render /site/pages/
+cp -R /static/* /site/
+quarto render /site/
