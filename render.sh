75719789c67c57e6bf80fb6778998640eb1e055e
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-10-11 09:25:20 -0700

    use yq to manipulate quarto yaml file

--- render.sh
+++ render.sh
@@ -10,6 +10,27 @@ if [[ $ORG == "hubverse-org" && $REPO == "hub-dashboard-predtimechart" ]]; then
 fi
 ROOT="https://raw.githubusercontent.com/$ORG/$REPO/refs/heads/$BRANCH/$DIR"
 
+# copy resources to the user's site
+echo "📂 Copying site skeleton"
 cp -R /static/* /site/pages/
+# modify the quarto to contain the pages and their ordering
+echo "  Updating site config"
+yq -i '
+  # load the user site config
+  load("/site/site-config.yml") as $cfg |
+
+  # Add the pages to the navigation bar
+  with(.website.navbar.left;
+    . += $cfg.pages
+  ) |
+  # Update the title
+  with(.website.title;
+    . |= $cfg.title
+  )
+' /site/pages/_quarto.yml
+# modify the predtimechart js to get content from the correct place
 sed -i -E "s+\{ROOT\}+$ROOT+" /site/pages/resources/predtimechart.js
+# render the site!
+echo "🏗  Building the site"
 quarto render /site/pages/
+echo "😃 All done!"
